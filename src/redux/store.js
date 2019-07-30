d71e6d3057387c939297f761f57bd1a1b742fe11
Max-F <max.fullmer@outlook.com>
2019-07-30 09:48:55 -0700

    Combined reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
 import reduxMiddleWare from 'redux-promise-middleware';
-import rootReducer from './reducer.js';
+
+import userReducer from './reducers/userReducer.js';
+import acousticDataReducer from './reducers/acousticDataReducer.js';
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    acousticData: acousticDataReducer
+})
 
 export default createStore(rootReducer, applyMiddleware(reduxMiddleWare));
