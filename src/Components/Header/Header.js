7d76b82ef90221bb370991df415a8efbffbebb68
Max-F <max.fullmer@outlook.com>
2019-07-31 22:00:05 -0700

    completed, but need to touch up on style wise

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getUserSession } from '../../redux/reducers/userReducer.js';
+import './Header.scss'
+import axios from 'axios'
 
-export default function Header() {
-    return (
-        <div>HEADER</div>
-    )
+class Header extends Component {
+    componentDidMount() {
+        axios.get('/api/user_session')
+        .then(response => {
+            console.log(response)
+            this.props.getUserSession(response.data)
+        })
+    }
+
+    render() {
+        // 
+        // header will contain logout and user home button if user IS logged in.
+        // onClick, logout button will take site-user back to login page "/"
+        // if on login page (AKA USER IS NOT LOGGED IN), header will hide buttons.
+        console.log(this.props.user)
+        return (
+            (!this.props.user) 
+            ?
+                <header>
+                    <img src="/public/trident.png" alt="trident.png" />
+                </header>
+            : 
+                <header>
+                    <Link to="/user/:user_id"><button>User Home</button> </Link> 
+                    <img src="/public/trident.png" alt="trident.png" />
+                    <Link to="/" ><button>Logout</button></Link>    
+                </header>
+        ) 
+    }
 }
+
+function mapReduxStateToProps(reduxState){
+    return {
+        user: reduxState.user
+    }
+}
+
+const mapDispatchToProps = {
+    getUserSession
+}
+
+export default connect(mapReduxStateToProps, mapDispatchToProps)(Header);
