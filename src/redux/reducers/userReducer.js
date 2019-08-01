a633ba1c1702887f3db9fcd4479c335f40ed55dc
Max-F <max.fullmer@outlook.com>
2019-07-31 21:58:43 -0700

    went to 1 reducer only 4 storing session in state

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,41 @@
+const GET_USER_SESSION = 'GET_USER_SESSION';
+const ATTEMPT_LOGIN = 'ATTEMPT_LOGIN';
+const REGISTER = 'REGISTER';
+const LOGOUT = 'LOGOUT';
+
+const initialState = {
+    user: "hello"
+}
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        case GET_USER_SESSION:
+            console.log(action.payload)
+            console.log(state.user)
+            return {user: action.payload};
+        case ATTEMPT_LOGIN:
+            return {user: action.payload}
+        case REGISTER:
+            return {}
+        case LOGOUT:
+            return {}
+        default:
+            return state;
+    }
+}
+
+// action creators:
+
+export function getUserSession(LoggedInUser){
+    return {
+        payload: LoggedInUser,
+        type: GET_USER_SESSION
+    }
+}
+
+export function attemptLogin(status) {
+    return {
+        payload: status,
+        type: ATTEMPT_LOGIN
+    }
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,7 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reduxMiddleWare from 'redux-promise-middleware';
 
 import userReducer from './reducers/userReducer.js';
-import acousticDataReducer from './reducers/acousticDataReducer.js';
 
-const rootReducer = combineReducers({
-    user: userReducer,
-    acousticData: acousticDataReducer
-})
 
-export default createStore(rootReducer, applyMiddleware(reduxMiddleWare));
+export default createStore(userReducer, applyMiddleware(reduxMiddleWare));
