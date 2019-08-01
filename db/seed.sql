b2335291214b12404e4909a6694c9585e4db8502
Max-F <max.fullmer@outlook.com>
2019-07-31 22:00:59 -0700

    July 31, 2019

--- db/seed.sql
+++ db/seed.sql
@@ -21,12 +21,14 @@ CREATE TABLE datadescription(
     construction BOOLEAN,
     vehicle BOOLEAN,
     environmental BOOLEAN,
+    file_type TEXT NOT NULL,
+    file_img TEXT DEFAULT NULL,
     upload_date DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-INSERT INTO datadescription (title, marineBio, construction, vehicle, environmental)
+INSERT INTO datadescription (title, marineBio, construction, vehicle, environmental, file_type, file_img)
 VALUES 
-('Bottlenose Dolphin 55kHz', TRUE, FALSE, FALSE, FALSE);
+('Bottlenose Dolphin 5-21kHz', TRUE, FALSE, FALSE, FALSE, 'png', 'https://www.researchgate.net/profile/Brian_Branstetter/publication/293820383/figure/fig1/AS:329766868668419@1455633956429/Signature-whistle-of-the-dolphin-SAY-A-Waveform-of-the-whistle-and-B-spectrogram-of.png');
 
 SELECT * FROM users;
 SELECT * FROM datadescription;
