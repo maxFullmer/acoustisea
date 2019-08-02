50c90426fd25f047cf6ba3ff57702ee8c3407294
Max-F <max.fullmer@outlook.com>
2019-08-02 16:57:02 -0700

    Logged in user can see form to post to dataInfo

--- server/controllers/dataInfoController.js
+++ server/controllers/dataInfoController.js
@@ -2,7 +2,7 @@ module.exports = {
     getDataInfo: async (req, res) => {
         const db = req.app.get('db');
         // either from req.session or req.body
-        const { user_id } = req.body;
+        const { user_id } = req.params;
 
         const userDataInfo = await db.get_user_data_info([user_id]).catch(err => console.log(err));
         console.log(userDataInfo)
@@ -11,8 +11,8 @@ module.exports = {
 
     addDataInfo: async (req, res) => {
         const db = req.app.get('db');
-        const { username, user_id, title, file_type, subtopicArray } = req.body;
-        const newPost = await db.add_data_info([username, user_id, title, file_type, ...subtopicArray])
+        const { username, user_id, title, file_type, subtopicArray, dataSummary } = req.body;
+        const [newPost] = await db.add_data_info([username, user_id, title, file_type, ...subtopicArray, dataSummary])
         .catch(err => console.log(err));
         console.log(newPost)
         res.status(200).send(newPost)
--- src/Components/UserData/UserData.js
+++ src/Components/UserData/UserData.js
@@ -8,64 +8,146 @@ class UserData extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            dataToDisplay: [],
             dataTitle: "",
             file_type: "",
-            isMarBio: false,
+            isMarBio: true,
             isVaV: false,
-            isConstrc: false,
-            isEnviron: false
+            isCivilEgr: false,
+            isEnviron: false,
+            dataSummary: ""
         }
     }
 
-    universalSubtopicHandler = (property) => {
+    componentDidMount() {
+        axios.get(`/api/user_data/${this.props.match.params.user_id}`)
+        .then(response => {
+            this.setState({
+                dataToDisplay: response.data
+            })
+        })
+    }
+
+    subtopicHandler = (event) => {
+        console.log(this.state.isMarBio)
         this.setState({
             isMarBio: false,
             isVaV: false,
-            isConstrc: false,
+            isCivilEgr: false,
             isEnviron: false,
-            [property]: true
+            [event.target.id]: true
+        })
+    }
+
+    textAreaHandler = (property, value) => {
+        this.setState({
+            [property]: value
         })
     }
 
     postDataDescription = () => {
         const { username, user_id } = this.props.user;
-        const { dataTitle, file_type, isMarBio, isVaV, isConstrc, isEnviron } = this.state;
-        const subtopicArray = [isMarBio, isVaV, isConstrc, isEnviron];
+        const { dataTitle, file_type, isMarBio, isVaV, isCivilEgr, isEnviron, dataSummary } = this.state;
+        const subtopicArray = [isMarBio, isVaV, isCivilEgr, isEnviron];
 
-        axios.post(`/api/user_data/:user_id`, { username: username, user_id: user_id, title: dataTitle, file_type: file_type, subtopicArray: subtopicArray})
+        axios.post(`/api/user_data_form`, 
+            {
+                username: username, 
+                user_id: user_id, 
+                title: dataTitle, 
+                file_type: file_type, 
+                subtopicArray: subtopicArray, 
+                dataSummary: dataSummary
+            })
+        .then(response => {
+            this.setState({
+                dataToDisplay: [ ...this.state.dataArray, response.data]
+            }) 
+        })
     }
 
     render() {
+        let { dataToDisplay } = this.state;
+        let mappedUserData = dataToDisplay.map((dataObj, index) => {
+            return (
+                <div key={index}>
+                    <ul>
+                        <li>{dataObj.title}</li>
+                        <li>{dataObj.file_type}</li>
+
+                        <li>{dataObj.marineBio ? "Marine Bioacoustics" : null}</li>
+                        <li>{dataObj.vehicle ? "Vessels & Vehicles" : null}</li>
+                        <li>{dataObj.civilEgr ? "Civil Engineering" : null}</li>
+                        <li>{dataObj.environmental ? "Environmental" : null}</li>
+
+                        <li>{dataObj.upload_date.slice(0,10)}</li>
+                        <li>{dataObj.description}</li>
+                    </ul>
+                </div>
+            )
+        })
+
+        console.log('Data To Display: ', dataToDisplay)
+        // console.log('session user: ', this.props.user)
+        // console.log('match params id from URL: ', this.props.match.params.user_id)
+        console.log('dataSummary: ', this.state.dataTitle)
+        console.log('dataSummary: ', this.state.file_type)
+        console.log('dataSummary: ', this.state.dataSummary)
         return (
             <div>
-                <form>
-                    <p>Please select the category for your data method:</p>
-                    <div>
-                        <input type="radio" id="categoryChoice1"
-                        name="category" value="isMarBio" 
-                        onClick={event => this.universalSubtopicHandler(event.target.value)}/>
-                        <label htmlFor="categoryChoice1">Marine Bioacoustics</label>
-                    
-                        <input type="radio" id="categoryChoice2"
-                        name="category" value="isVaV" 
-                        onClick={event => this.universalSubtopicHandler(event.target.value)}/>
-                        <label htmlFor="categoryChoice2">Vessels {"&"} Vehicles</label>
-                    
-                        <input type="radio" id="categoryChoice3"
-                        name="category" value="isConstrc" 
-                        onClick={event => this.universalSubtopicHandler(event.target.value)}/>
-                        <label htmlFor="categoryChoice3">Construction</label>
-                        
-                        <input type="radio" id="categoryChoice3"
-                        name="category" value="isEnviron" 
-                        onClick={event => this.universalSubtopicHandler(event.target.value)}/>
-                        <label htmlFor="categoryChoice3">Environmental</label>
-                    </div>
+                <div>{mappedUserData}</div>
+                <div>{
+                    (this.props.user !== null 
+                    && this.props.user.user_id === +this.props.match.params.user_id
+                    ) 
+                    ? 
+                    (
+                    <form>
+                        <p>Title {"*"}:</p>
+                        <div>
+                        <textarea name="dataTitle" rows="1" cols="45" 
+                            onChange={event => this.textAreaHandler(event.target.name, event.target.value)} />    
+                        </div>
 
-                    <div>
-                    <button type="submit">Submit</button>
-                    </div>
-                </form>
+                        <p>Filename extension (i.e.: .zip, .png, .mat, ...) {"*"}:</p>
+                        <div>
+                        <textarea name="file_type" rows="1" cols="7" 
+                            onChange={event => this.textAreaHandler(event.target.name, event.target.value)} />    
+                        </div>
+                        
+                        <p>Category {"*"}:</p>
+                        <div>
+                            <input type="radio" name="category" id="isMarBio" checked={this.state.isMarBio}
+                            onChange={event => this.subtopicHandler(event)}/>
+                            <label htmlFor="isMarBio">Marine Bioacoustics</label>
+                        
+                            <input type="radio" name="category" id="isVaV" checked={this.state.isVaV}
+                            onChange={event => this.subtopicHandler(event)}/>
+                            <label htmlFor="isVaV">Vessels {"&"} Vehicles</label>
+                        
+                            <input type="radio" name="category" id="isCivilEgr" checked={this.state.isCivilEgr}
+                            onChange={event => this.subtopicHandler(event)}/>
+                            <label htmlFor="isCivilEgr">Civil Engineering</label>
+                            
+                            <input type="radio" name="category" id="isEnviron" checked={this.state.isEnviron}
+                            onChange={event => this.subtopicHandler(event)}/>
+                            <label htmlFor="isEnviron">Environmental</label>
+                        </div>
+
+                        <p>Please add a description of your data</p>
+                        <div>
+                            <textarea name="dataSummary" rows="5" cols="45" 
+                            onChange={event => this.textAreaHandler(event.target.name, event.target.value)} />
+                        </div>
+
+                        <div>
+                            <button type="submit" onSubmit={this.postDataDescription}>Submit</button>
+                        </div>
+                    </form> 
+                    )
+                    :
+                    null}
+                </div>
             </div> 
         );
     }
