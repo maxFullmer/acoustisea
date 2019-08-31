6d7eb7156c15fdc069eb1c2c8e781a08cda42957
Max-F <max.fullmer@outlook.com>
2019-08-31 09:18:27 -0700

    removed file size limit

--- server/index.js
+++ server/index.js
@@ -22,8 +22,8 @@ const {
 } = process.env;
 
 const app = express();
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static( `${__dirname}/../build` ) );
 
 massive(CONNECTION_STRING).then(db => {
