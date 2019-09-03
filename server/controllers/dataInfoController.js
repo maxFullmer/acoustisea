dc043aebe5b0f0f18f24dc189d6ab87f257e6d26
Max-F <max.fullmer@outlook.com>
2019-09-02 21:39:27 -0700

    console logs removed

--- server/controllers/dataInfoController.js
+++ server/controllers/dataInfoController.js
@@ -1,5 +1,4 @@
 module.exports = {
-    // Data Info
     getUserDataInfo: async (req, res) => {
         const db = req.app.get('db');
         const { user_id } = req.params;
@@ -13,22 +12,20 @@ module.exports = {
     addUserDataInfo: async (req, res) => {
         const db = req.app.get('db');
         const { username, user_id, title, file_type, subtopic, dataSummary, s3link, s3key } = req.body;
-        console.log(req.body)
+        
         const newPost = await db.add_user_data_info([username, user_id, title, file_type, subtopic, dataSummary, s3link, s3key])
         .catch(err => console.log(err));
-        console.log(newPost)
+
         res.status(200).send(newPost[newPost.length - 1])
     },
 
     updateUserDataInfo: async (req, res) => {
         const db = req.app.get('db');
         const { title, file_type, subtopic, dataSummary, data_id, user_id } = req.body;
-        console.log('req.body', req.body)
 
         const [updatedDataInfo] = await db.update_user_data_info([data_id, title, file_type, subtopic, dataSummary, user_id])
         .catch(err => console.log(err));
 
-        console.log('updated data info: ', updatedDataInfo)
         res.status(200).send(updatedDataInfo)
     },
 
@@ -43,21 +40,12 @@ module.exports = {
         res.status(200).send(diminishedUserData)
     },
 
-    //Public Data
-
     getSubtopicDataInfo: async (req, res) => {
         const db = req.app.get('db');
         const { subtopicSelected } = req.params;
-        console.log(subtopicSelected)
 
-        //error within sql statement VVV
         const subtopicData = await db.get_subtopic_data_info([subtopicSelected])
         .catch(err => console.log(err));
-
-        console.log(subtopicData)
-        
-        // console.log(subtopicData)
-        // console.log(publicData)
 
         res.status(200).send(subtopicData);
     }
